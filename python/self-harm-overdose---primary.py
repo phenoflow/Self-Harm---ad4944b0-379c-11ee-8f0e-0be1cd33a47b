000dcbf415e59ed831a77f538389763d5e7743f3
Martin Chapman <contact@martinchapman.co.uk>
2023-08-10 17:47:01 +0100

    Update made by Phenoflow

---
+++ python/self-harm-overdose---primary.py
@@ -0,0 +1,25 @@
+# Matthew J Carr, Darren M Ashscroft, Evangelos Kontopantelis, David While, Yvonne Awenant, Jayne Cooper, Carolyn Chew-Graham, Nav Kapur, Roger T Webb, 2023.
+
+import sys, csv, re
+
+codes = [{"code":"SL...15","system":"readv2"},{"code":"U200.11","system":"readv2"},{"code":"U200.12","system":"readv2"},{"code":"U200.13","system":"readv2"},{"code":"U202.12","system":"readv2"},{"code":"U202.13","system":"readv2"},{"code":"U202.15","system":"readv2"},{"code":"U202.16","system":"readv2"},{"code":"U202.17","system":"readv2"},{"code":"U202.18","system":"readv2"},{"code":"U204.12","system":"readv2"},{"code":"U204.13","system":"readv2"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('self-harm-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["self-harm-overdose---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["self-harm-overdose---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["self-harm-overdose---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
